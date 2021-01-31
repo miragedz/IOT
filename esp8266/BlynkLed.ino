d1abe31368057192bc518711d78b90ccc0120d77
miragedz <kingrachid2000@gmail.com>
2021-01-31 12:32:52 +0100

    Update BlynkLed.ino

--- esp8266/BlynkLed.ino
+++ esp8266/BlynkLed.ino
@@ -2,13 +2,7 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-
 char auth[ ] = "YourAuthToken";
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
 
 char ssid[ ] = "YourNetworkName";
 char pass[ ] = "YourPassword";
