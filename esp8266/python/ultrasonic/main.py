4c2bf9a94674101682f79cc8591536340947b422
miragedz <kingrachid2000@gmail.com>
2021-03-01 20:56:02 +0100

    Update main.py

--- esp8266/python/ultrasonic/main.py
+++ esp8266/python/ultrasonic/main.py
@@ -2,6 +2,12 @@ import machine
 import hcsr04
 import time
 
+# GPIO  Boarde
+# 14    D5
+# 12    D6
+# 13    D7
+# 15    D8
+
 ultrasonic = hcsr04.HCSR04(trigger_pin=13, echo_pin=12, echo_timeout_us=1000000)
 led = machine.Pin(2, machine.Pin.OUT)
 buzzer = machine.PWM(machine.Pin(15, machine.Pin.OUT))
