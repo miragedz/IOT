fa101b6b67f12f88f075bad9ffdd33d425ad3e3f
miragedz <kingrachid2000@gmail.com>
2023-09-07 20:53:04 +0100

    Update wifi-to-eth-route.sh

--- Raspi/wifi-to-eth-route.sh
+++ Raspi/wifi-to-eth-route.sh
@@ -12,11 +12,11 @@
 # Don't forget to change the name of network interface
 # Check them with `ifconfig`
 
-ip_address="192.168.2.1"
-netmask="255.255.255.0"
+ip_address_and_network_mask_in_CDIR_notation="192.168.2.1/24"
 dhcp_range_start="192.168.2.2"
 dhcp_range_end="192.168.2.100"
 dhcp_time="12h"
+dns_server="1.1.1.1"
 eth="eth0"
 wlan="wlan0"
 
@@ -30,7 +30,9 @@ sudo iptables -A FORWARD -i $eth -o $wlan -j ACCEPT
 
 sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 
-sudo ifconfig $eth $ip_address netmask $netmask
+sudo ip link set $eth down
+sudo ip link set $eth up
+sudo ip addr add  $ip_address_and_network_mask_in_CDIR_notation dev $eth 
 
 # Remove default route created by dhcpcd
 sudo ip route del 0/0 dev $eth &> /dev/null
@@ -39,11 +41,11 @@ sudo systemctl stop dnsmasq
 
 sudo rm -rf /etc/dnsmasq.d/* &> /dev/null
 
-echo -e "interface=$eth\n\
-bind-interfaces\n\
-server=8.8.8.8\n\
-domain-needed\n\
-bogus-priv\n\
+echo -e "interface=$eth
+bind-interfaces
+server=$dns_server
+domain-needed
+bogus-priv
 dhcp-range=$dhcp_range_start,$dhcp_range_end,$dhcp_time" > /tmp/custom-dnsmasq.conf
 
 sudo cp /tmp/custom-dnsmasq.conf /etc/dnsmasq.d/custom-dnsmasq.conf
